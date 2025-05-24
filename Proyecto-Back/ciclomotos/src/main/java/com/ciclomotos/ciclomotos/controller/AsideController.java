792bd52cb2b49ab2dc7988b33544b5075c356d1d
zCarreado <sergio.perez06@uptc.edu.co>
2025-05-23 20:07:47 -0500

    Mapeado del Aside

---
+++ Proyecto-Back/ciclomotos/src/main/java/com/ciclomotos/ciclomotos/controller/AsideController.java
@@ -0,0 +1,29 @@
+package com.ciclomotos.ciclomotos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AsideController {
+    @GetMapping("/venta")
+    public String mostrarVenta() {
+        return "venta";
+    }
+    @GetMapping("/clientes")
+    public String mostrarClientes() {
+        return "registro-cliente";
+    }
+    @GetMapping("/inventario")
+    public String mostrarInventario() {
+        return "inventario";
+    }
+    @GetMapping("/reportes")
+    public String mostrarReportes() {
+        return "reportes";
+    }
+
+    @GetMapping("/usuarios")
+    public String mostrarUsuiario() {
+        return "registro-cliente";
+    }
+}
